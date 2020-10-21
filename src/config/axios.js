429a26485780e4cb5b6cf738cd68f2d0a3999093
abhishek sappandi <abhisheksappandi@gmail.com>
2020-10-21 16:19:28 +0530

    deploy to heroku

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,10 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:3333/api'
+    baseURL: 'https://node-backend-ilkalsarees.herokuapp.com/api'
 })
 
 export default axios
+
+// http://localhost:3333/api
+// https://node-backend-ilkalsarees.herokuapp.com/api
